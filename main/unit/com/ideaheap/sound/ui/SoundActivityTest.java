737b7586db52b2f37704f24794ff854489582ce0
Nicholas Wertzberger <wertnick@gmail.com>
2012-11-03 12:37:39 -0500

    Trying to integrate unit tests with this thing. I should have done this a long time ago...

---
+++ main/unit/com/ideaheap/sound/ui/SoundActivityTest.java
@@ -0,0 +1,14 @@
+package com.ideaheap.sound.ui;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SoundActivityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
